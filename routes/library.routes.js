c731c8c5a1935231b295af3a429f3f2f54d02961
Tamara Rott <tam.midnight@gmail.com>
2021-12-09 19:56:04 +0100

    added post and get library route

--- routes/library.routes.js
+++ routes/library.routes.js
@@ -1,27 +1,35 @@
 const router = require("express").Router();
 const Library = require("../models/Library.model");
+const User = require("../models/User.model");
 
-router.post("/create-library", (req, res) => {
+router.post("/create-library", async (req, res) => {
   const { title, description, publicLibrary } = req.body;
   const _id = req.session.loggedInUser._id;
 
   if (!title) {
-    res.status(500).json({
+    res.status(400).json({
       errorMessage: "Please enter a title",
     });
     return;
   }
-
-  Library.create({ title, description, publicLibrary, user: _id })
-    .then((library) => {
-      res.status(200).json(library);
-    })
-    .catch((err) => {
-      res.status(500).json({
-        errorMessage: "Something went wrong!",
-        message: err,
-      });
+  try {
+    let library = await Library.create({
+      title,
+      description,
+      publicLibrary,
+      user: _id,
     });
+    await User.findByIdAndUpdate(
+      { _id },
+      { $addToSet: { libraries: library._id } }
+    );
+    res.status(200).json(library);
+  } catch (err) {
+    res.status(500).json({
+      errorMessage: "Something went wrong!",
+      message: err,
+    });
+  }
 });
 
 router.get("/library/:id", (req, res) => {
@@ -40,12 +48,12 @@ router.get("/library/:id", (req, res) => {
     });
 });
 
-router.post("/library/:id", (req, res) => {
+router.patch("/library/:id", (req, res) => {
   const { id: _id } = req.params;
   const { title, description, publicLibrary } = req.body;
 
   if (!title) {
-    res.status(500).json({
+    res.status(400).json({
       errorMessage: "Please enter a title",
     });
     return;
@@ -62,5 +70,7 @@ router.post("/library/:id", (req, res) => {
       });
     });
 });
+
+router.delete("/library/:id/delete", (req, res) => {});
 
 module.exports = router;
